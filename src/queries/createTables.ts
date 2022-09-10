af554a820426d2ef95b2d79baa94ec1e0083d4a1
Diego Brito <diegorbrito9@gmail.com>
2022-09-09 22:56:50 -0300

    new: criando a query para as tabelas base

---
+++ src/queries/createTables.ts
@@ -0,0 +1,24 @@
+export default `
+  CREATE TABLE IF NOT EXISTS Restaurant(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    logo VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    opening_hours VARCHAR(255) NOT NULL
+  );
+ 
+ CREATE TABLE IF NOT EXISTS Category(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL
+ );
+
+  CREATE TABLE IF NOT EXISTS Product(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    product_image VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    restaurantId INT,
+    categoryId INT,
+    FOREIGN KEY (restaurantId) REFERENCES Restaurant(id),
+    FOREIGN KEY (categoryId) REFERENCES Category(id)
+  );
+`;
