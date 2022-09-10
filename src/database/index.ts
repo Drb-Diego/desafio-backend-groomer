f3aa09dd6b90454b1a36c079f12eea974f1a93dc
Diego Brito <diegorbrito9@gmail.com>
2022-09-09 22:56:00 -0300

    new: criando conexão com o banco de dados e criando um '.env.example'

---
+++ src/database/index.ts
@@ -0,0 +1,19 @@
+import { createPool } from 'mysql2/promise';
+
+const {
+  DATABASE_USER,
+  DATABASE_PASSWORD,
+  DATABASE_HOST,
+  DATABASE_PORT,
+  DATABASE_NAME,
+} = process.env;
+
+const connection = createPool({
+  user: DATABASE_USER,
+  password: DATABASE_PASSWORD,
+  host: DATABASE_HOST,
+  port: Number(DATABASE_PORT),
+  database: DATABASE_NAME,
+});
+
+export default connection;
