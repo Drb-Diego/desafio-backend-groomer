591b81bee4928f76df37f7d3b9454c0b9b313836
Diego Brito <diegorbrito9@gmail.com>
2022-09-10 00:45:33 -0300

    new: criando diretorio em queries para cada entidade

--- src/models/restaurantModel.ts
+++ src/models/restaurantModel.ts
@@ -1,6 +1,6 @@
 import { IRestaurantInfo } from "../@types";
 import connection from "../database";
-import createRestaurantSql from "../queries/createRestaurantSql";
+import createRestaurantSql from "../queries/restaurant/createRestaurantSql";
 
 const createRestaurant = async ({ name, openingHours, imageBlob }: IRestaurantInfo) => {
   const sql = createRestaurantSql;
